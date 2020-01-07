f1c76028a5d883bb34accdeae6212e9087b62dbb
Student <student@localhosts>
2020-01-07 11:06:09 +0300

    Added Increse & Special chashback

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 const regularP = 300000;
 const RegP = 0.01
 const caash = regularP * RegP;
+
+const incPur = 10000
+const incPurPer = 0.05
+
+
+const spePur = 5000
+const spePurPer = 0.03
+
+let caash = regularP * RegP + incPur *incPurPer + 
+spePur * spePurPer
+
+
 const caashlimi = 3000
 if (caash > caashlimi)
 {
